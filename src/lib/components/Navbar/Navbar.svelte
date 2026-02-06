8072f1e0a6cffaffea40259138fe91bc0bd2ba70
Michael Real <michaeldreal15@gmail.com>
2026-02-06 13:46:08 +0800

    feat: add navbar

---
+++ src/lib/components/Navbar/Navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { page } from '$app/state';
+    import { resolve } from '$app/paths';
+</script>
+
+<nav class="sticky top-0 z-10 w-full">
+    <ul class="text-c-white justify-center flex h-16 gap-x-3">
+        <li>
+            <a  class="h-full items-center flex px-5" href={resolve('/')}>About</a>
+        </li>
+        <li>
+            <a  class="h-full items-center flex px-5" href={resolve('/')}>About</a>
+        </li>
+        <li>
+            <a  class="h-full items-center flex px-5" href={resolve('/')}>About</a>
+        </li>
+    </ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,6 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<div class="bg-c-black w-full min-h-screen">
+	{@render children()}
+</div>
